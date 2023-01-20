ce4bc68afd82f13792baf3db2b71c32745997fcf
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-01-20 17:04:26 +0100

    appheader li and mixins

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
 export default {
   name: 'vite-comics',
   data() {
     return {
       title: '',
     }
+  },
+  components: {
+    AppHeader
   }
 }
 </script>
 
 <template>
+  <AppHeader />
 </template>
 
-<style>
-
+<style lang="scss">
+@use './assets/scss/style.scss'
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,59 +3,59 @@ export default {
     name: 'AppHeader',
     data() {
         return {
-            navObj: [
-                [
-                    {
-                        text: 'Characters',
-                        url: '#',
-                        current: false,
-                    },
-                    {
-                        text: 'Comics',
-                        url: '#',
-                        current: true,
-                    },
-                    {
-                        text: 'Movies',
-                        url: '#',
-                        current: false,
-                    },
-                    {
-                        text: 'TV',
-                        url: '#',
-                        current: false,
-                    },
-                    {
-                        text: 'Games',
-                        url: '#',
-                        current: false,
-                    },
-                    {
-                        text: 'Collectibles',
-                        url: '#',
-                        current: false,
-                    },
-                    {
-                        text: 'Videos',
-                        url: '#',
-                        current: false,
-                    },
-                    {
-                        text: 'Fans',
-                        url: '#',
-                        current: false,
-                    },
-                    {
-                        text: 'News',
-                        url: '#',
-                        current: false,
-                    },
-                    {
-                        text: 'Shop',
-                        url: '#',
-                        current: false,
-                    },
-                ]
+            links: [
+
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+
 
             ]
         }
@@ -64,9 +64,27 @@ export default {
 </script>
 
 <template>
-
+    <header class="container">
+        <figure>
+            <a href="#">
+                <img src="../assets/img/dc-logo.png" alt="DC_Logo">
+            </a>
+        </figure>
+        <nav>
+            <ul>
+                <li v-for="link in links" :key="link.text">
+                    <a :href="link.url">{{ link.text }}</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
-<style>
+<style lang="scss" scoped>
+ul {
 
+    li {
+        list-style-type: none;
+    }
+}
 </style>
