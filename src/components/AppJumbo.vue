ae1184b48b89c43d1ce3e98b58f87347cc701c9f
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-01-20 17:42:36 +0100

    AppJumbo update (blackboard)

--- src/components/AppJumbo.vue
+++ src/components/AppJumbo.vue
@@ -37,9 +37,36 @@ export default {
 </script>
 
 <template>
+    <main>
+        <section id="contents-space">
+            <div class="blackboard">
+                <h3>--> Content goes here </h3>
+            </div>
+        </section>
 
+        <section>
+            <nav>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+        </section>
+    </main>
 </template>
 
-<style>
+<style lang="scss" scoped>
+@use '../assets/scss/partials/mixins' as *;
 
+#contents-space {
+    .blackboard {
+        @include flex-between;
+        min-height: 100px;
+        background-color: black;
+
+        h3 {
+            color: white;
+            margin-left: 15rem;
+        }
+    }
+}
 </style>
